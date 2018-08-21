bfb40182f051dda4942ba705f764f533df51bb75
Alkotsuki <saidex@me.com>
2018-08-21 12:26:00 +0300

    Initial Commit

---
+++ ImageDownloader/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ImageDownloader
+//
+//  Created by Pavel Koval on 8/21/18.
+//  Copyright © 2018 Alkotsuki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
